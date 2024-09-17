5034304d666f25ff0039d50287644fcf9f03ff17
Adam Sadler <adambsadler@gmail.com>
2024-09-17 15:27:20 -0400

    Initial Commit

---
+++ TasteTestTests/TasteTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TasteTestTests.swift
+//  TasteTestTests
+//
+//  Created by Adam Sadler on 9/17/24.
+//
+
+import Testing
+@testable import TasteTest
+
+struct TasteTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
